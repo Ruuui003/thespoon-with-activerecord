131f36502e053475c4c1384579f5e2f1b0cdd159
Agnes Selvita <a.selvita@gmail.com>
2021-03-30 10:49:49 +0800

    Rails crud

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :restaurants #, only: [:index, :show]
+
+  # verb '', to: 'controller#action', as: ....
+  # get '/restaurants', to: 'restaurants#index', as: 'restaurants'
+  # get '/restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
+  # get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  # post '/restaurants', to: 'restaurants#create'
+
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: 'edit_restaurant'
+
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # put '/restaurants/:id', to: 'restaurants#update'
+
+  # delete '/restaurants/:id', to: 'restaurants#destroy', as: 'destroy_restaurant'
+end
